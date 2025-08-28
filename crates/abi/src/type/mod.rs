a7ed36185676c9738e8980a49c13a42c98cb0331
Owen Bellish <owenbellish@gmail.com>
2025-08-28 12:25:18 -0400

    remove those

--- crates/abi/src/type/mod.rs
+++
@@ -1,9 +0,0 @@
-mod id;
-mod info;
-mod lut;
-
-pub use self::{
-	id::{ArrayTypeId, HasStaticTypeId, PointerTypeId, TypeId},
-	info::{HasStaticTypeName, TypeDefinition, TypeDefinitionData},
-	lut::TypeLut
-};
